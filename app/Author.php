bd25137d6aaadaf876a85740f2be8e5147a0cecb
yaso614 <kebinkastora@naver.com>
2018-12-30 14:20:20 +0900

    database&model

---
+++ app/Author.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    public $timestamps = false;
+    protected $fillable = ['email', 'password'];
+}
+
+
+
---
+++ app/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public $timestamps = false;
+    protected $fillable = ['title','body'];
+}
+
+
